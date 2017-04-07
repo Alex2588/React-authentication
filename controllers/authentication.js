d1b8ecc9c2c8ff2920753acd9c53333db7d894bf
Alex2588 <alexander_beschasnyy@mail.ru>
2017-04-07 14:00:49 +0300

    Completed server-side authentication

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,10 @@ function generateToken(user){
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 };
 
+exports.signin = (req, res, next) => {
+    res.send({token: generateToken(req.user)});
+};
+
 exports.signup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
